615e894506aa2c4cca8eae7f70575f2931198339
joaaovitoorsantos <joaaovitoorsantos@gmail.com>
2023-08-16 00:31:27 -0300

    done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,13 @@
-import { Fragment } from 'react'
-import Head from 'next/head'
-import '@styles/globals.css'
-import '@shopify/polaris/build/esm/styles.css'
-import type { AppProps } from 'next/app'
-import enTranslations from '@shopify/polaris/locales/en.json'
 import { AppProvider } from '@shopify/polaris'
+import '@shopify/polaris/build/esm/styles.css'
+import enTranslations from '@shopify/polaris/locales/en.json'
+import '@styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Fragment } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
-      <Head>
-        <title>Abode</title>
-        <meta name="description" content="Criado por João Vitor" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <AppProvider i18n={enTranslations}>
         <Component {...pageProps} />
       </AppProvider>
