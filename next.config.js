e1caacf155e8f75af173cd3293a509da528361c9
joaaovitoorsantos <joaaovitoorsantos@gmail.com>
2023-08-16 20:33:16 -0300

    done

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
 }
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+
 const withPWA = require('next-pwa')
 module.exports = withPWA({
   pwa: {
