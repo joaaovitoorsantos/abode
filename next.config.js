2be28d51d0658b91e27f5e275868dbf4b10c40da
joaaovitoorsantos <joaaovitoorsantos@gmail.com>
2023-08-16 20:27:54 -0300

    done

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import withPWA from 'next-pwa'
+const withPWA = require('next-pwa')
 
-export default withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    // outras opções de configuração PWA aqui
-  },
-  // outras configurações do Next.js aqui, como reactStrictMode, etc.
+module.exports = withPWA({
   reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+  },
 })
