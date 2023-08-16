8898c0e6c82c193507243c44022a7e3dd6388b72
joaaovitoorsantos <joaaovitoorsantos@gmail.com>
2023-08-16 20:25:59 -0300

    done

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const withPWA = require('next-pwa')
+import withPWA from 'next-pwa'
 
-module.exports = withPWA({
+export default withPWA({
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     dest: 'public',
